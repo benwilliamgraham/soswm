08be583e56267334563e5d93321369af1b3d7784
Ben Graham <benwilliamgraham@gmail.com>
2020-05-02 01:13:47 -0400

    Circular stack

--- soswm.c
+++ soswm.c
@@ -10,6 +10,9 @@ Display *dpy;
 Screen *scr;
 Window root;
 Atom WM_PROTOCOLS, WM_DELETE_WINDOW;
+FILE *log_fd;
+
+const int gaps = 8;
 
 typedef struct SWindow SWindow;
 struct SWindow {
@@ -20,7 +23,6 @@ struct SWindow {
 typedef struct SWorkspace SWorkspace;
 struct SWorkspace {
   SWindow *win_stack;
-  size_t num_wins;
   SWorkspace *prev, *next;
 } * wksp_stack;
 
@@ -62,7 +64,7 @@ Bool send_event(Window win, Atom proto) {
 }
 
 int x_error(Display *dpy, XErrorEvent *err) {
-  printf("ERROR\n");
+  fprintf(log_fd, "Error\n");
   char error_text[1024];
   XGetErrorText(dpy, err->error_code, error_text, sizeof(error_text));
   fprintf(stderr, "X error: %s\n", error_text);
@@ -70,40 +72,38 @@ int x_error(Display *dpy, XErrorEvent *err) {
 }
 
 /* -- Window configuration -- */
-Bool find_window(Window win, SWorkspace **wksp_loc, SWindow **win_loc) {
-  printf("FINDING\n");
-  for (*wksp_loc = wksp_stack; *wksp_loc; *wksp_loc = (*wksp_loc)->next) {
-    for (*win_loc = (*wksp_loc)->win_stack; *win_loc;
-         *win_loc = (*win_loc)->next) {
-      if ((*win_loc)->win == win)
-        return True;
-    }
-  }
-  return False;
-}
-
 void update_windows(SWorkspace *wksp) {
-  printf("UPDATING, %ld\n", wksp->num_wins);
+  fprintf(log_fd, "UPDATING\n");
   SMonitor *mon = mon_stack;
-  if (wksp->num_wins == 1) {
-    XSetInputFocus(dpy, wksp->win_stack->win, RevertToNone, CurrentTime);
+  if (wksp->win_stack == wksp->win_stack->next) {
     XMoveResizeWindow(dpy, wksp->win_stack->win, mon->x, mon->y, mon->w,
                       mon->h);
-  } else if (wksp->num_wins) {
-    XSetInputFocus(dpy, wksp->win_stack->win, RevertToNone, CurrentTime);
-    XMoveResizeWindow(dpy, wksp->win_stack->win, mon->x, mon->y, mon->w / 2,
-                      mon->h);
-    size_t i = 0;
-    for (SWindow *win = wksp->win_stack->next; win; win = win->next, i++) {
-      XMoveResizeWindow(dpy, win->win, mon->x + mon->w / 2,
-                        mon->h * i / (wksp->num_wins - 1), mon->w / 2,
-                        mon->h / (wksp->num_wins - 1));
+  } else {
+    Bool v = True;
+    unsigned int x = mon->x + gaps / 2, y = mon->y + gaps / 2,
+                 w = mon->w - gaps, h = mon->h - gaps;
+    SWindow *win;
+    for (win = wksp->win_stack; win->next != wksp->win_stack; win = win->next) {
+      if (v) {
+        XMoveResizeWindow(dpy, win->win, x + gaps / 2, y + gaps / 2,
+                          w / 2 - gaps, h - gaps);
+        w /= 2;
+        x += w;
+      } else {
+        XMoveResizeWindow(dpy, win->win, x + gaps / 2, y + gaps / 2, w - gaps,
+                          h / 2 - gaps);
+        h /= 2;
+        y += h;
+      }
+      v = !v;
     }
+    XMoveResizeWindow(dpy, win->win, x + gaps / 2, y + gaps / 2, w - gaps,
+                      h - gaps);
   }
 }
 
 void configure_request(const XConfigureRequestEvent *e) {
-  printf("CONFIG\n");
+  fprintf(log_fd, "CONFIG, %ld\n", e->window);
   /* configure window normally; TODO update this */
   XWindowChanges changes;
   changes.x = e->x;
@@ -114,46 +114,75 @@ void configure_request(const XConfigureRequestEvent *e) {
   changes.sibling = e->above;
   changes.stack_mode = e->detail;
   XConfigureWindow(dpy, e->window, e->value_mask, &changes);
+  XSync(dpy, False);
 }
 
 void map_request(XMapRequestEvent *e) {
-  printf("MAP\n");
+  fprintf(log_fd, "MAP, %ld\n", e->window);
   /* push the new window */
   SWindow *new = malloc(sizeof(SWindow));
   new->win = e->window;
-  new->prev = NULL;
-  new->next = wksp_stack->win_stack;
-  if (wksp_stack->win_stack)
-    wksp_stack->win_stack->prev = new;
-  wksp_stack->win_stack = new;
-  wksp_stack->num_wins++;
+  if (wksp_stack->win_stack) {
+    new->prev = wksp_stack->win_stack->prev;
+    new->next = wksp_stack->win_stack;
+  } else
+    new->prev = new->next = new;
+  wksp_stack->win_stack = new->prev->next = new->next->prev = new;
   update_windows(wksp_stack);
 
-  /* draw updated window */
-  XMapWindow(dpy, e->window);
+  /* map and focus updated window */
+  XMapWindow(dpy, new->win);
+  XSetInputFocus(dpy, new->win, RevertToPointerRoot, CurrentTime);
 }
 
 void destroy_notify(XDestroyWindowEvent *e) {
-  printf("DESTROY\n");
-  SWorkspace *wksp = NULL;
-  SWindow *win = NULL;
-  if (!find_window(e->window, &wksp, &win))
-    return;
-  if (win->prev)
-    win->prev->next = win->next;
-  if (win->next)
-    win->next->prev = win->prev;
-  if (wksp->win_stack == win)
-    wksp->win_stack = win->next;
-  free(win);
-  wksp->num_wins--;
+  fprintf(log_fd, "DESTROY\n");
+  for (SWorkspace *wksp = wksp_stack; wksp;
+       wksp = (wksp->next != wksp_stack) ? wksp->next : NULL) {
+    for (SWindow *win = wksp->win_stack; win;
+         win = (win->next != wksp->win_stack) ? win->next : NULL) {
+      if (win->win == e->window) {
+        if (win == win->next)
+          wksp->win_stack = NULL;
+        else {
+          win->next->prev = win->prev;
+          win->prev->next = win->next;
+          if (wksp->win_stack == win) {
+            wksp->win_stack = win->next;
+            XSetInputFocus(dpy, win->next->win, RevertToNone, CurrentTime);
 
-  /* TODO ensure window is visible before redrawing */
-  update_windows(wksp);
+            /* TODO make workspace is visible */
+            update_windows(wksp);
+          }
+        }
+        free(win);
+        return;
+      }
+    }
+  }
 }
 
 /* -- Keyboard interaction -- */
-void new_wksp(__attribute__((unused)) XKeyPressedEvent *e) {
+void window_quit(XKeyPressedEvent *e) {
+  if (e->subwindow != None && !send_event(e->subwindow, WM_DELETE_WINDOW))
+    XKillClient(dpy, e->subwindow);
+}
+
+void window_roll_l(__attribute__((unused)) XKeyPressedEvent *e) {
+  if (wksp_stack->win_stack)
+    wksp_stack->win_stack = wksp_stack->win_stack->next;
+}
+
+void window_roll_r(__attribute__((unused)) XKeyPressedEvent *e) {
+  if (wksp_stack->win_stack)
+    wksp_stack->win_stack = wksp_stack->win_stack->prev;
+}
+
+void window_swap(XKeyPressedEvent *e) {}
+
+void window_push(XKeyPressedEvent *e) {}
+
+void workspace_new(__attribute__((unused)) XKeyPressedEvent *e) {
   /* if the top workspace is empty, do nothing */
   if (!wksp_stack->win_stack)
     return;
@@ -165,12 +194,15 @@ void new_wksp(__attribute__((unused)) XKeyPressedEvent *e) {
   wksp_stack = wksp;
 }
 
-void quit(XKeyPressedEvent *e) {
-  if (e->subwindow != None && !send_event(e->subwindow, WM_DELETE_WINDOW))
-    XKillClient(dpy, e->subwindow);
-}
+void workspace_roll_l(__attribute__((unused)) XKeyPressedEvent *e) {}
 
-void logout(__attribute__((unused)) XKeyPressedEvent *e) {
+void workspace_roll_r(__attribute__((unused)) XKeyPressedEvent *e) {}
+
+void workspace_swap(XKeyPressedEvent *e) {}
+
+void wm_restart(__attribute__((unused)) XKeyPressedEvent *e) {}
+
+void wm_logout(__attribute__((unused)) XKeyPressedEvent *e) {
   XCloseDisplay(dpy);
   exit(0);
 }
@@ -188,19 +220,59 @@ void editor(__attribute__((unused)) XKeyPressedEvent *e) {
 }
 
 struct {
-  unsigned int mask, keysym, keycode;
+  unsigned int mask, key, keycode;
   void (*handler)(XKeyPressedEvent *);
 } keybinds[] = {
-    {.mask = Mod1Mask, .keysym = XK_n, .handler = new_wksp},
-    {.mask = Mod1Mask | ShiftMask, .keysym = XK_l, .handler = logout},
-    {.mask = Mod1Mask | ShiftMask, .keysym = XK_q, .handler = quit},
-    {.mask = Mod1Mask, .keysym = XK_space, .handler = launcher},
-    {.mask = ShiftMask, .keysym = XK_t, .handler = terminal},
-    {.mask = Mod1Mask, .keysym = XK_e, .handler = editor}};
-const size_t NUM_KEYBINDS = sizeof(keybinds) / sizeof(*keybinds);
+    /* Window management */
+    {.mask = Mod4Mask, .key = XK_q, .handler = window_quit},
+    {.mask = Mod4Mask, .key = XK_j, .handler = window_roll_l},
+    {.mask = Mod4Mask, .key = XK_k, .handler = window_roll_r},
+    {.mask = Mod4Mask, .key = XK_1, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_2, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_3, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_4, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_5, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_6, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_7, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_8, .handler = window_swap},
+    {.mask = Mod4Mask, .key = XK_9, .handler = window_swap},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_1, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_2, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_3, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_4, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_5, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_6, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_7, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_8, .handler = window_push},
+    {.mask = Mod4Mask | ShiftMask, .key = XK_9, .handler = window_push},
+
+    /* Workspace management */
+    {.mask = Mod4Mask | ControlMask, .key = XK_n, .handler = workspace_new},
+    {.mask = Mod4Mask | ControlMask, .key = XK_j, .handler = workspace_roll_l},
+    {.mask = Mod4Mask | ControlMask, .key = XK_k, .handler = workspace_roll_r},
+    {.mask = Mod4Mask | ControlMask, .key = XK_1, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_2, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_3, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_4, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_5, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_6, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_7, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_8, .handler = workspace_swap},
+    {.mask = Mod4Mask | ControlMask, .key = XK_9, .handler = workspace_swap},
+
+    /* WM management */
+    {.mask = Mod4Mask | Mod1Mask, .key = XK_r, .handler = wm_restart},
+    {.mask = Mod4Mask | Mod1Mask, .key = XK_l, .handler = wm_logout},
+
+    /* Launchers */
+    {.mask = Mod4Mask, .key = XK_space, .handler = launcher},
+    {.mask = Mod4Mask, .key = XK_t, .handler = terminal},
+    {.mask = Mod4Mask, .key = XK_e, .handler = editor},
+};
+const unsigned int NUM_KEYBINDS = sizeof(keybinds) / sizeof(*keybinds);
 
 void key_press(XKeyPressedEvent *e) {
-  for (size_t i = 0; i < NUM_KEYBINDS; i++) {
+  for (unsigned int i = 0; i < NUM_KEYBINDS; i++) {
     if ((keybinds[i].mask & e->state) && (keybinds[i].keycode == e->keycode))
       keybinds[i].handler(e);
   }
@@ -213,6 +285,7 @@ void motion_notify(__attribute__((unused)) XMotionEvent *e) {}
 
 /* -- Main loop -- */
 int main() {
+  log_fd = fopen("/tmp/soswm.log", "w");
   /* initialize display */
   if (!(dpy = XOpenDisplay(0x0))) {
     fprintf(stderr, "soswm: Cannot open display\n");
@@ -239,7 +312,6 @@ int main() {
   /* create initial workspace */
   wksp_stack = malloc(sizeof(SWorkspace));
   wksp_stack->win_stack = NULL;
-  wksp_stack->num_wins = 0;
   wksp_stack->prev = NULL;
   wksp_stack->next = NULL;
 
@@ -248,16 +320,16 @@ int main() {
   WM_DELETE_WINDOW = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
 
   /* bind keys */
-  for (size_t a = 0; a < NUM_KEYBINDS; a++) {
-    keybinds[a].keycode = XKeysymToKeycode(dpy, keybinds[a].keysym);
-    XGrabKey(dpy, keybinds[a].keycode, keybinds[a].mask, root, True,
+  for (unsigned int i = 0; i < NUM_KEYBINDS; i++) {
+    keybinds[i].keycode = XKeysymToKeycode(dpy, keybinds[i].key);
+    XGrabKey(dpy, keybinds[i].keycode, keybinds[i].mask, root, True,
              GrabModeAsync, GrabModeAsync);
   }
 
   /* bind buttons */
-  XGrabButton(dpy, 1, NoEventMask, root, True,
-              ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
-              GrabModeAsync, GrabModeAsync, None, None);
+  /* XGrabButton(dpy, 1, NoEventMask, root, True, */
+  /* ButtonPressMask | ButtonReleaseMask | PointerMotionMask, */
+  /* GrabModeAsync, GrabModeAsync, None, None); */
 
   /* run startup programs */
   launch((char *[]){"feh", "--bg-scale",
